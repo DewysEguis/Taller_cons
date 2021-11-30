8133aac762bc5a7d6b52d9854f998833c1e71754
Dewys Eguis <deguis@mangus.org>
2021-11-30 18:22:57 -0500

    punto 1

---
+++ Taller_cons.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+
+url = 'covid_22_noviembre.csv'
+data = pd.read_csv(url, low_memory=False)
+
+data.rename(columns={'ID de caso': 'ID'}, inplace=True)
+
+data['Ubicación del caso'].replace('CASA', 'Casa', inplace=True)
+data['Recuperado'].replace('fallecido', 'Fallecido', inplace=True)
+data['Tipo de contagio'].replace('relacionado', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('RELACIONADO', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('EN ESTUDIO', 'En estudio', inplace=True)
+data['Tipo de contagio'].replace('En Estudio', 'En estudio', inplace=True)
+data['Sexo'].replace('m', 'M', inplace=True)
+data['Sexo'].replace('f', 'F', inplace=True)
+
+# Punto 1 
+print('numero de casos de contagio: ' + str(len(data)))
